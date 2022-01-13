034e4bf6d5fcf66fbe4232972c4024dc6904b65f
olga urazlova <ourazlova@awg.ru>
2022-01-13 19:12:30 +0300

    Add hero to the center of the display Add events of ship control left and right using keys

--- ship.py
+++ ship.py
@@ -8,7 +8,19 @@ class Ship:
 
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
+        # Корабль появляется внизу экрана
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Флаг перемещения
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Обновление позиции корабля с учетом флага перемещения"""
+        if self.moving_right:
+            self.rect.x += 1
+        elif self.moving_left:
+            self.rect.x -= 1
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
