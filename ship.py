2f6dae9a5b1cffd6737f5cb4443d95080d4d46dc
olga urazlova <ourazlova@awg.ru>
2022-01-14 16:26:39 +0300

    Add ship speed control Ship stop on the edge of the window Exit game with q key

--- ship.py
+++ ship.py
@@ -2,14 +2,22 @@ import pygame
 
 
 class Ship:
+    """Класс для управления кораблем"""
+
     def __init__(self, ai_game):
+        """Инициализация корабля и его начальной позиции"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
-        # Корабль появляется внизу экрана
+
+        # Корабль отображается внизу игоровой области
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Координата центра корабля
+        self.x = float(self.rect.x)
 
         # Флаг перемещения
         self.moving_right = False
@@ -17,10 +25,14 @@ class Ship:
 
     def update(self):
         """Обновление позиции корабля с учетом флага перемещения"""
-        if self.moving_right:
-            self.rect.x += 1
-        elif self.moving_left:
-            self.rect.x -= 1
+        # Обновляется атрибут x объекта ship, не rect
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        elif self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # Обновление атрибута rect на основе self.x
+        self.rect.x = self.x
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
