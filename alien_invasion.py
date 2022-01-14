505c63afd8edb47cc27f5e374d62d01bf87a33df
olga urazlova <ourazlova@awg.ru>
2022-01-14 16:53:15 +0300

    Added fullscreen setting

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,10 +7,13 @@ from hero import Hero
 
 class AlienInvasion:
     def __init__(self):
+        """Инициализирует игру и создает игровые ресурсы"""
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
